b839f62dd222710f3abfe8e5155449251eda66fe
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-27 04:04:26 -0400

    clean up

--- examples.js
+++
@@ -1,6 +0,0 @@
-var parse = require('parse-git-config');
-var remote = require('remote-origin-url');
-
-var url = remote.sync();
-console.log(parse.sync())
-console.log(url)
--- updatefile.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function (app, base, env) {
-  app.option('pkg')
-};
