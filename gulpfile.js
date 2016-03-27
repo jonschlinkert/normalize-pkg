49531099c11e5028d6b32fb84a3787d0ca759a41
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-27 08:25:02 -0400

    update deps, bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
+var unused = require('gulp-unused');
 var eslint = require('gulp-eslint');
 
 var lint = ['index.js', 'lib/**/*.js'];
@@ -23,6 +24,12 @@ gulp.task('lint', function() {
   return gulp.src(lint.concat(['test/*.js', 'gulpfile.js']))
     .pipe(eslint())
     .pipe(eslint.format());
+});
+
+gulp.task('unused', function() {
+  var keys = Object.keys(require('./lib/utils.js'));
+  return gulp.src(['*.js', 'lib/**/*.js'])
+    .pipe(unused({keys: keys}));
 });
 
 gulp.task('default', ['test', 'lint']);
