dbc5105a3057989c9547b63bf7bf9cb17c255037
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-27 08:21:45 -0400

    update cli

--- cli.js
+++ cli.js
@@ -2,10 +2,24 @@
 
 var path = require('path');
 var argv = require('minimist')(process.argv.slice(2));
-var create = require('./');
-var schema = create();
+var writeJson = require('write-json');
+var Normalizer = require('./');
+var config = new Normalizer();
 
-var pkg = require(path.resolve(process.cwd(), 'package.json'));
-var res = schema.normalize(pkg);
+/**
+ * Optionally specific a destination path
+ */
 
-console.log(res);
+var dest = argv.dest || argv.d || 'package.json';
+
+/**
+ * Write the file
+ */
+
+writeJson(dest, config.normalize(), function(err) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('done');
+});
